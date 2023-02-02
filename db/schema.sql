108c8c16fb7f0dbf6f6b3df859d6ca2b7499adca
Herman <evanaherman@gmail.com>
2023-02-01 22:16:09 -0800

    update schema and added index.js

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROB DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE
+
+-- Create Table Department
+
+-- Create Table role
+
+-- Create Table employee
